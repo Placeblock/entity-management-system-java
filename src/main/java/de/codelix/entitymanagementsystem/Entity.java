78bcd007f19ee4da4bcb653ea1fd2201c258121f
Felix <felix@weglehner.info>
2025-02-06 17:24:51 +0100

    Init Commit

---
+++ src/main/java/de/codelix/entitymanagementsystem/Entity.java
@@ -0,0 +1,16 @@
+package de.codelix.entitymanagementsystem;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ * The identity of a single person. Multiple accounts of different
+ * Applications may be linked to one entity and requested if necessary.
+ */
+@Getter
+@RequiredArgsConstructor
+public class Entity {
+    private final UUID uuid;
+}
